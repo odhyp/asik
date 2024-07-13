3041ea05ef03d83afd08d7cd4ece67248685b838
odhyp <pradhana.odhy@gmail.com>
2024-07-13 18:26:20 +0700

    refactor: update main.py using local imports

--- main.py
+++ main.py
@@ -1,41 +1,17 @@
-import pandas as pd
+from src.csv_handler import CSVHandler
+from src.excel_handler import ExcelHandler
 
 input_path = "data/sample.xlsx"
 output_path = "data/sample.csv"
-output_json = "data/sample.json"
-
-
-def read_excel(input_path):
-    excel_file = pd.read_excel(input_path, index_col=0, header=0)
-    print(excel_file)
-
-
-def convert_to_csv(input_path, output_path):
-    excel_file = pd.read_excel(input_path)
-    excel_file.to_csv(output_path, index=False, header=True)
-
-
-def read_csv(csv_path):
-    df = pd.read_csv(csv_path, sep=",", index_col=0, header=0)
-
-    # Check column names
-    # print("Column names in the CSV file:", df.columns.tolist())
-
-    for index, row in df.iterrows():
-        value_nip = row['nip']
-        value_nama = row['nama']
-        value_gapok = row['gapok']
-
-        print(f"NIP : {value_nip}")
-        print(f"Nama : {value_nama}")
-        print(f"Gapok : {value_gapok}")
-        print("\n")
 
 
 def main():
-    # read_excel(input_path)
-    # convert_to_csv(input_path, output_path)
-    read_csv(output_path)
+    excel_handler = ExcelHandler()
+    excel_handler.read_excel(input_path)
+
+    csv_handler = CSVHandler()
+    csv_handler.convert_to_csv(input_path, output_path)
+    csv_handler.read_csv(output_path)
 
 
 if __name__ == '__main__':
