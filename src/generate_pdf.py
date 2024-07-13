8bf68d275ef41e49ffaeea0dd344c06bafd2ab77
odhyp <pradhana.odhy@gmail.com>
2024-07-13 22:40:32 +0700

    feat: add header and footer

--- src/generate_pdf.py
+++ src/generate_pdf.py
@@ -1,9 +1,22 @@
 from fpdf import FPDF
 
 
-class GeneratePDF:
+class GeneratePDF():
+
+    class PDF(FPDF):
+        def header(self):
+            self.set_font("helvetica", "B", 15)
+            self.cell(80)
+            self.cell(30, 10, "Title", border=1, align="C")
+            self.ln(20)
+
+        def footer(self):
+            self.set_y(-15)
+            self.set_font("helvetica", "B", 15)
+            self.cell(0, 10, f"Page {self.page_no()}/{{nb}}", align="C")
+
     def create_pdf(self, output_path):
-        pdf = FPDF()
+        pdf = self.PDF()
         pdf.add_page()
         pdf.set_font("Helvetica", size=16)
         pdf.cell(40, 10, "Hello World!")
