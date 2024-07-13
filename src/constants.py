c4f07beb5b126aa0c27abe76abd554f21a371fee
odhyp <pradhana.odhy@gmail.com>
2024-07-14 06:39:18 +0700

    feat: add constants.py

---
+++ src/constants.py
@@ -0,0 +1,16 @@
+# PDF Generations
+
+# Show cell borders (used for testing)
+SHOW_BORDERS = False
+
+# Page size (mm). Default size for F4 paper
+PAPER_SIZE = (215, 330)
+
+# Page margins (mm). Top, bottom, left, and right margin
+PAPER_MARGINS = 25.4
+
+# Font Family. Chose either "times" or "helvetica".
+FONT_FAMILY = "times"
+
+# Font size. Default is 12
+FONT_SIZE = 12
