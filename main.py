bd5b85cf93aae264fa1299b2f1bfcf7539daabf5
odhyp <pradhana.odhy@gmail.com>
2024-07-14 10:46:58 +0700

    refactor: update generate_pdf to use constants

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from src.csv_handler import CSVHandler
 from src.excel_handler import ExcelHandler
 from src.generate_pdf import GeneratePDF
 
+from src.constants import PAPER_SIZE
+
 input_path = "data/sample.xlsx"
 output_path = "data/sample.csv"
 output_pdf = "data/sample.pdf"
@@ -15,7 +17,10 @@ def main():
     csv_handler.convert_to_csv(input_path, output_path)
     csv_handler.read_csv(output_path)
 
-    generate_pdf = GeneratePDF()
+    generate_pdf = GeneratePDF(orientation='P',
+                               unit='mm',
+                               format=PAPER_SIZE)
+    generate_pdf.set_margin(0)
     generate_pdf.create_pdf(output_pdf)
 
 
