c6c6c355c0f481efd800cd8dfa11a6b40785273f
odhyp <pradhana.odhy@gmail.com>
2024-07-16 07:34:35 +0700

    refactor: remove column name checker

--- src/csv_handler.py
+++ src/csv_handler.py
@@ -2,9 +2,6 @@ import pandas as pd
 
 
 class CSVHandler:
-    def check_column_names(self, csv_path):
-        print("Column names in the CSV file:", df.columns.tolist())
-
     def convert_to_csv(self, input_path, output_path):
         excel_file = pd.read_excel(input_path)
         excel_file.to_csv(output_path, index=False, header=True)
