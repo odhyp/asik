1759ff9eea2e9b1952b16061f8fc10a8a2b7a933
odhyp <pradhana.odhy@gmail.com>
2024-08-06 23:11:27 +0700

    test: add generate_report function

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.csv_handler import CSVHandler
 from src.excel_handler import ExcelHandler
 from src.generate_pdf import GeneratePDF
+from src.generate_report import create_report
 
 input_path = "data/sample.xlsx"
 output_path = "data/sample.csv"
@@ -17,6 +18,9 @@ def main():
 
     generate_pdf = GeneratePDF()
     generate_pdf.create_pdf(output_pdf)
+
+    output_path = "output/sample_output.html"
+    create_report(output_path=output_path)
 
 
 if __name__ == '__main__':
