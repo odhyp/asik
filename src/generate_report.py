5766623ab40f06caee7c2adfd8e2fab0fb5d9a9f
odhyp <pradhana.odhy@gmail.com>
2024-08-07 14:22:06 +0700

    refactor: update variables using jinja variables

--- src/generate_report.py
+++ src/generate_report.py
@@ -9,8 +9,49 @@ def create_report(output_path):
 
     # 2 - Compiled data for injecting
     context = {
-        "data_image": data_image,
-        "data_date": data_date
+        "header_pemda": "Pemerintah Daerah Daerah Istimewa Yogyakarta",
+        "header_opd": "Badan Pengelola Keuangan dan Aset",
+        "header_upt": "KPPD DIY Di Kabupaten Bantul",
+        "header_address": "Jalan Urip Sumoharjo, Bantul",
+        "header_telp": "0274-367483",
+        "header_kodepos": "55711",
+
+        "title_period": "Agustus 2024",
+
+        "employee_name": employee_name,
+        "employee_nip": employee_nip,
+        "employee_gol": employee_gol,
+        "employee_status": employee_status,
+
+        "salary_gapok": salary_gapok,
+        "salary_tunjistri": salary_tunjistri,
+        "salary_tunjanak": salary_tunjanak,
+        "salary_tunjstruk": salary_tunjstruk,
+        "salary_tunjberas": salary_tunjberas,
+        "salary_tunjpph": salary_tunjpph,
+        "salary_pembulatan": salary_pembulatan,
+        "salary_jumlahkotor": salary_jumlahkotor,
+
+        "salary_iwp8": salary_iwp8,
+        "salary_iwp1": salary_iwp1,
+        "salary_pph": salary_pph,
+
+        "salary_bpd": salary_bpd,
+        "salary_korpri": salary_korpri,
+        "salary_dw": salary_dw,
+        "salary_zakat": salary_zakat,
+        "salary_pelita": salary_pelita,
+        "salary_beras": salary_beras,
+        "salary_lain": salary_lain,
+        "salary_jumlahpot": salary_jumlahpot,
+
+        "final_bersih": final_bersih,
+        "final_terbilang": final_terbilang,
+
+        "signature_place": "Bantul",
+        "signature_date": signature_date,
+        "signature_name": signature_name,
+        "signature_nip": signature_nip,
     }
 
     # 3 - Create jinja template object from file
